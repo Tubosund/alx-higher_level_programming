ead80cc1c59f3aaace1a62113b313433da0548b3
Olatunbosun Oladayo <tubosund@gmail.com>
2023-06-06 15:24:00 -0700

    done

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 def remove_char_at(string, n):
     if n < 0 or n >= len(string):
         return string
 
-    result = " "
+    result = ""
     for i in range(len(string)):
         if i != n:
             result += string[i]
