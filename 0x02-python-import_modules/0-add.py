ca4cecf2e6944a239ca0820d1f7002ac52a5b6d7
Olatunbosun Oladayo <tubosund@gmail.com>
2023-06-08 17:26:25 -0700

    d

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
+if__name__=="__main__"
 a = 1
 b = 2
+from add_0 import add
 result = add(a, b)
-print("{0} + {1} = {2}".format(a, b, result))
+print("{} + {} = {}".format(a, b, result))
