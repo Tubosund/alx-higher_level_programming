4369e6d3ed52ab76e53a7737402e909049899b6a
Olatunbosun Oladayo <tubosund@gmail.com>
2023-06-15 16:16:19 -0700

    done

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+
 def weight_average(my_list=[]):
-    if my_list and len(my_list):
+    if not my_list:
+        return 0
         num = 0
         dem = 0
         for tup in my_list:
             num += (tup[0] * tup[1])
             dem += tup[1]
         return (num / dem)
-    return None
+
