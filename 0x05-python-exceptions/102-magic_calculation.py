9778343a3137c7d8cedfbbe4aa84b46d07f54de0
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-06-27 15:13:58 -0700

    done

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -8,10 +8,9 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
 
-            result += (a ** b) / i
-
+            result += a ** b / i
         except:
-            result += a + b
+            result = b + a
             break
 
     return result
