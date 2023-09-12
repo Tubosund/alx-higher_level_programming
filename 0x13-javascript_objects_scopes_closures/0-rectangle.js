c9350707dea31efb8a4ecb41d8c840c94413bd6e
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-09-12 16:14:30 -0700

    kk

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
+
 class Rectangle {
-	constructor() {
-		// This is an empty constructor
-	}
+  constructor() {
+    // This is an empty constructor
+  }
 }
+
 module.exports = Rectangle;
+
